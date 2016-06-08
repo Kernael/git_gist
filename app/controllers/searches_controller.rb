06d7209aadb28d809523bd04f7313dd6cf65a0d9
Eliot BARDET <eliot.bardet@gmail.com>
2016-06-08 12:34:40 +0100

    base search logic

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,34 @@
+class SearchesController < ApplicationController
+  before_action :set_search, only: [:show]
+
+  def new
+    @search = Search.new
+  end
+
+  def create
+    @search = Search.new
+
+    respond_to do |format|
+      if @search.save
+        format.html { redirect_to @search, notice: 'Search result :' }
+        format.json { render :show, status: :created, location: @search }
+      else
+        format.html { render :new }
+        format.json { render json: @search.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_search
+    @search = Search.find(params[:id])
+  end
+
+  def search_params
+    params.require(:search).permit(:username, :gist_id)
+  end
+end
